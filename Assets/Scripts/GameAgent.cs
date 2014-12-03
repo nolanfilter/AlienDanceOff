39e204c33d33dd4b541fc416a4e624841b64d61d
Nolan Filter <nfilter@ogmento.com>
2014-12-03 16:18:19 -0500

    Initial commit

---
+++ Assets/Scripts/GameAgent.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameAgent : MonoBehaviour {
+
+	public enum Points
+	{
+		Head = 0,
+		Butt = 1,
+		Shoulder = 2,
+		Invalid = 3,
+	}
+
+	public enum Parts
+	{
+		Tail = 0,
+		Eyestalk = 1,
+		SleepyFriend = 2,
+		Invalid = 3,
+	}
+	
+	private Dictionary<Points, Parts> pointToPartMap = new Dictionary<Points, Parts>();
+	private int[] partValues = new int[ Enum.GetNames( typeof( Parts ) ).Length - 1 ];
+	private int numPoints = Enum.GetNames( typeof( Points ) ).Length - 1;
+
+	private static GameAgent mInstance = null;
+	public static GameAgent instance
+	{
+		get
+		{
+			return mInstance;
+		}
+	}
+
+	void Awake()
+	{
+		if( mInstance != null )
+		{
+			Debug.LogError( string.Format( "Only one instance of GameAgent allowed! Destroying:" + gameObject.name +", Other:" + mInstance.gameObject.name ) );
+			Destroy( gameObject );
+			return;
+		}
+		
+		mInstance = this;
+	}
+
+	void Start()
+	{
+		//TODO procedurally set points/parts map based on connections
+		pointToPartMap.Add( Points.Head, Parts.Eyestalk );
+		pointToPartMap.Add( Points.Butt, Parts.Tail );
+		pointToPartMap.Add( Points.Shoulder, Parts.SleepyFriend );
+	}
+
+	void Update()
+	{
+		string currentInputString = SerialAgent.SerialReadLine();
+		int inputValue;
+
+		for( int i = 0; i < currentInputString.Length; i++ )
+			if( int.TryParse( "" + currentInputString[i], out inputValue ) )
+				EvaluateInputValue( inputValue );
+
+		for( int i = 0; i < partValues.Length; i++ )
+			partValues[i] = Mathf.RoundToInt( Mathf.Clamp01( partValues[i] ) );
+
+		foreach( KeyValuePair<Points, Parts> kvp in pointToPartMap )
+			Debug.Log( "" + kvp.Value + " = " + partValues[ (int)pointToPartMap[ kvp.Key ] ] );
+	}
+
+	private void EvaluateInputValue( int inputValue )
+	{
+		Points point = (Points)( inputValue / numPoints );
+		int relativeValue = inputValue%2;
+
+		if( pointToPartMap.ContainsKey( point ) )
+			partValues[ (int)pointToPartMap[ point ] ] += ( relativeValue == 0 ? -1 : 1 );
+	}
+}
---
+++ Assets/Scripts/SerialAgent.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+
+public class SerialAgent : MonoBehaviour {
+
+	public string portName = "/dev/tty.usbmodem1411";
+
+	private SerialPort serial;
+
+	private static SerialAgent mInstance = null;
+	public static SerialAgent instance
+	{
+		get
+		{
+			return mInstance;
+		}
+	}
+	
+	void Awake()
+	{
+		if( mInstance != null )
+		{
+			Debug.LogError( string.Format( "Only one instance of SerialAgent allowed! Destroying:" + gameObject.name +", Other:" + mInstance.gameObject.name ) );
+			Destroy( gameObject );
+			return;
+		}
+		
+		mInstance = this;
+	}
+
+	void Start()
+	{
+		try 
+		{
+			serial = new SerialPort();
+			
+			serial.ReadBufferSize = 8192;
+			serial.WriteBufferSize = 128;
+			
+			serial.PortName = portName;
+			serial.BaudRate = 9600;
+			serial.Parity = Parity.None;
+			serial.StopBits = StopBits.One;
+			
+			serial.Open();
+		}
+		catch (Exception ex) 
+		{
+
+		}
+	}
+
+	public static string SerialReadLine()
+	{
+		if( instance )
+			return instance.internalSerialReadLine();
+
+		return "";
+	}
+
+	private string internalSerialReadLine()
+	{
+		string serialLine = "";
+
+		try 
+		{
+			serialLine = serial.ReadLine();
+		}
+		catch (Exception ex)
+		{
+			
+		}
+
+		return serialLine;
+	}
+}
