1f8a99c68cd8f54ad2c459d3ad71982658796737
Nolan Filter <nolanfilter@172-16-28-39.dynapool.nyu.edu>
2014-12-12 12:15:52 -0500

    Added textures and game logic

--- Assets/Scripts/GameAgent.cs
+++ Assets/Scripts/GameAgent.cs
@@ -30,7 +30,22 @@ public class GameAgent : MonoBehaviour {
 	}
 	private Difficulty currentDifficulty = Difficulty.Easy;
 
-	public AudioClip music = null;
+	public enum ScreenType
+	{
+		Freeze = 0,
+		GameOver = 1,
+		Googlies = 2,
+		ShakeTail = 3,
+		Shoulder = 4,
+		Sleepy = 5,
+		WelcomeTo = 6,
+		Invalid = 7,
+	}
+
+	public AudioClip[] musicClips;
+	public GUIStyle textStyle;
+	public SpriteRenderer[] screenSprites = new SpriteRenderer[ Enum.GetNames( typeof( ScreenType ) ).Length - 1 ];
+
 	private AudioSource audioSource = null;
 
 	private bool isDancing = false;
@@ -48,6 +63,10 @@ public class GameAgent : MonoBehaviour {
 	private string alienBodyPartName = "";
 
 	private int numInputValuesRemembered = 25;
+
+	private bool hasStarted = false;
+
+	private ScreenType nextScreenTypeToDisplay = ScreenType.Invalid;
 
 	private static GameAgent mInstance = null;
 	public static GameAgent instance
@@ -75,19 +94,23 @@ public class GameAgent : MonoBehaviour {
 		partsFrequencyRanges[ (int)Parts.Tail ] = new Vector2( 0.66f, 1f );
 		partsFrequencyRanges[ (int)Parts.Eyestalk ] = new Vector2( 0.33f, 0.66f );
 		partsFrequencyRanges[ (int)Parts.SleepyFriend ] = new Vector2( 0f, 0.33f );
+
+		for( int i = 0; i < screenSprites.Length; i++ )
+			screenSprites [i].enabled = false;
 	}
 
 	void Start()
 	{
-		if( music != null )
+		if( musicClips.Length > 0 )
 		{
 			audioSource = Camera.main.gameObject.AddComponent<AudioSource>();
 
 			audioSource.loop = true;
-			audioSource.clip = music;
 		}
 
 		SetDesiredPointPartPairs();
+
+		screenSprites[ (int)ScreenType.WelcomeTo ].enabled = true;
 	}
 
 	void Update()
@@ -108,6 +131,35 @@ public class GameAgent : MonoBehaviour {
 
 			if( inputValuesRememberedDelta > 0 )
 				pointValues[i].RemoveRange( 0, inputValuesRememberedDelta );
+		}
+
+		if( !hasStarted )
+		{
+			for( int i = 0; i < pointValues.Length; i++ )
+			{
+				int count = pointValues[i].Count;
+				
+				float frequency = 0f;
+				
+				if( count > 0 )
+				{
+					int numValuesOn = 0;
+					
+					for( int j = 0; j < count; j++ )
+						if( pointValues[i][j] == 1 )
+							numValuesOn++;
+					
+					frequency = (float)numValuesOn / (float)count;
+				}
+				
+				if( frequency > 0.5f )
+				{
+					hasStarted = true;
+					screenSprites[ (int)ScreenType.WelcomeTo ].enabled = false;
+				}
+			}
+			
+			return;
 		}
 
 		if( Input.GetKeyDown( KeyCode.Space ) )
@@ -136,6 +188,12 @@ public class GameAgent : MonoBehaviour {
 		}
 
 		displayString = "";
+
+		for( int i = 0; i < (int)currentDifficulty; i++ )
+			displayString += "\t";
+
+		displayString += currentDifficulty + "\n\n";
+
 		isDancing = true;
 
 		foreach( KeyValuePair<Points, Parts> kvp in desiredPointPartPairs )
@@ -143,57 +201,76 @@ public class GameAgent : MonoBehaviour {
 			if( !actualPointPartPairs.ContainsKey( kvp.Key ) || ( actualPointPartPairs.ContainsKey( kvp.Key ) && actualPointPartPairs[ kvp.Key ] != kvp.Value ) )
 			{
 				isDancing = false;
-				displayString += "Connect " + kvp.Value + " to " + kvp.Key + "\n";
+				displayString += "Connect " + kvp.Value + " to " + kvp.Key + "\n\n";
+			}
+			else
+			{
+				displayString += "\t\t" + kvp.Value + " connected to " + kvp.Key + "\n\n";
 			}
 		}
 
 		if( isDancing && !wasDancing )
 		{
-			alienBodyPartName = "";
-
-			for( int i = 0; i < UnityEngine.Random.Range( 5, 12 ); i++ )
-				alienBodyPartName += alphabet[ UnityEngine.Random.Range( 0, alphabet.Length ) ];
-
 			if( audioSource )
 				audioSource.Play();
+
+			foreach( KeyValuePair<Points, Parts> kvp in desiredPointPartPairs )
+				pointValues[ (int)kvp.Key ].Clear();
+
+			if( nextScreenTypeToDisplay == ScreenType.Googlies )
+				StartCoroutine( "DisplayGooglies" );
+			else if( nextScreenTypeToDisplay != ScreenType.Invalid )
+				screenSprites[ (int)nextScreenTypeToDisplay ].enabled = true;
 		}
 
 		if( !isDancing && wasDancing )
 		{
 			if( audioSource )
 				audioSource.Pause();
+
+			for( int i = 0; i < screenSprites.Length; i++ )
+				screenSprites [i].enabled = false;
 		}
+
+		screenSprites[ (int)ScreenType.Freeze ].enabled = ( currentDifficulty == Difficulty.None );
 
 		if( isDancing )
 		{
 			if( currentDifficulty == Difficulty.None )
-				displayString = "Taking a break.";
-			else
-				displayString = "DANCE YOUR " + alienBodyPartName + " OFF!!!!!!\n\n";
-
-			foreach( KeyValuePair<Points, Parts> kvp in actualPointPartPairs )
 			{
-				int count = pointValues[ (int)kvp.Key ].Count;
-				
-				float frequency = 0f;
-				
-				if( count > 0 )
+				displayString = "Taking a break.";
+			}
+			else
+			{
+				displayString = "\tDANCE YOUR " + alienBodyPartName + " OFF!!!!!!\n\n";
+
+				foreach( KeyValuePair<Points, Parts> kvp in desiredPointPartPairs )
 				{
-					int numValuesOn = 0;
+					int count = pointValues[ (int)kvp.Key ].Count;
 					
-					for( int i = 0; i < count; i++ )
-						if( pointValues[ (int)kvp.Key ][i] == 1 )
-							numValuesOn++;
+					float frequency = 0f;
 					
-					frequency = (float)numValuesOn / (float)count;
+					if( count > 0 )
+					{
+						int numValuesOn = 0;
+						
+						for( int i = 0; i < count; i++ )
+							if( pointValues[ (int)kvp.Key ][i] == 1 )
+								numValuesOn++;
+						
+						frequency = (float)numValuesOn / (float)count;
+					}
+					
+					if( frequency < partsFrequencyRanges[ (int)kvp.Value ].x )
+						displayString += "" + kvp.Value + " is too slow!\n\n";
+					else if( frequency > partsFrequencyRanges[ (int)kvp.Value ].y )
+						displayString += "\t\t" + kvp.Value + " is too fast!\n\n";
+					else
+						displayString += "\t" + kvp.Value + " is good!\n\n";
+
+					if( kvp.Value == Parts.SleepyFriend && frequency > partsFrequencyRanges[ (int)Parts.SleepyFriend ].y )
+						StartCoroutine( "DisplayGameOver" );
 				}
-				
-				if( frequency < partsFrequencyRanges[ (int)kvp.Value ].x )
-					displayString += "" + kvp.Value + " is too slow!\n\n";
-				else if( frequency > partsFrequencyRanges[ (int)kvp.Value ].y )
-					displayString += "" + kvp.Value + " is too fast!\n\n";
-				else
-					displayString += "" + kvp.Value + " is good!\n\n";
 			}
 		}
 
@@ -202,7 +279,10 @@ public class GameAgent : MonoBehaviour {
 
 	void OnGUI()
 	{
-		GUI.Label( new Rect( 5, 5, 500, 500 ), displayString );
+		if( screenSprites[ (int)ScreenType.WelcomeTo ].enabled || screenSprites[ (int)ScreenType.Googlies ].enabled || screenSprites[ (int)ScreenType.GameOver ].enabled )
+			return;
+
+		GUI.Label( new Rect( 5, 5, 500, 500 ), displayString, textStyle );
 	}
 
 	private void EvaluateInputValue( char inputValue )
@@ -261,10 +341,85 @@ public class GameAgent : MonoBehaviour {
 			possiblePoints.Remove( pointPartPair.Key );
 			possibleParts.Remove( pointPartPair.Value );
 		}
+
+		alienBodyPartName = "";
+		
+		for( int i = 0; i < UnityEngine.Random.Range( 5, 12 ); i++ )
+			alienBodyPartName += alphabet[ UnityEngine.Random.Range( 0, alphabet.Length ) ];
+
+		if( audioSource )
+			audioSource.clip = musicClips[ UnityEngine.Random.Range( 0, musicClips.Length ) ];
+
+		List<ScreenType> possibleScreensToDisplay = new List<ScreenType>();
+		
+		if( desiredPointPartPairs.ContainsKey( Points.Shoulder ) )
+			possibleScreensToDisplay.Add( ScreenType.Shoulder );
+		
+		if( desiredPointPartPairs.ContainsValue( Parts.Tail ) )
+			possibleScreensToDisplay.Add( ScreenType.ShakeTail );
+		
+		if( desiredPointPartPairs.ContainsValue( Parts.Eyestalk ) )
+			possibleScreensToDisplay.Add( ScreenType.Googlies );
+		
+		if( desiredPointPartPairs.ContainsValue( Parts.SleepyFriend ) )
+			possibleScreensToDisplay.Add( ScreenType.Sleepy );
+
+		if( possibleScreensToDisplay.Count > 0 )
+			nextScreenTypeToDisplay = possibleScreensToDisplay[ UnityEngine.Random.Range( 0, possibleScreensToDisplay.Count ) ];
+		else
+			nextScreenTypeToDisplay = ScreenType.Invalid;
+
+		for( int i = 0; i < screenSprites.Length; i++ )
+			screenSprites [i].enabled = false;
+
+		if( audioSource )
+			audioSource.Stop();
+
+		wasDancing = false;
 	}
 
 	private KeyValuePair<Points, Parts> RandomPointPartPair( List<Points> points, List<Parts> parts )
 	{
 		return new KeyValuePair<Points, Parts>( points[ UnityEngine.Random.Range( 0, points.Count ) ], parts[ UnityEngine.Random.Range( 0, parts.Count ) ] );
+	}
+
+	private IEnumerator DisplayGooglies()
+	{
+		bool[] spriteEnabled = new bool[ screenSprites.Length ];
+
+		for( int i = 0; i < screenSprites.Length; i++ )
+		{
+			spriteEnabled[i] = screenSprites[i].enabled;
+			screenSprites[i].enabled = false;
+		}
+
+		screenSprites[ (int)ScreenType.Googlies ].enabled = true;
+
+		yield return new WaitForSeconds( 2.5f );
+
+		screenSprites[ (int)ScreenType.Googlies ].enabled = false;
+
+		for( int i = 0; i < screenSprites.Length; i++ )
+			screenSprites[i].enabled = spriteEnabled[i];
+	}
+
+	private IEnumerator DisplayGameOver()
+	{
+		bool[] spriteEnabled = new bool[ screenSprites.Length ];
+		
+		for( int i = 0; i < screenSprites.Length; i++ )
+		{
+			spriteEnabled[i] = screenSprites[i].enabled;
+			screenSprites[i].enabled = false;
+		}
+
+		screenSprites[ (int)ScreenType.GameOver ].enabled = true;
+		
+		yield return new WaitForSeconds( 2.5f );
+		
+		screenSprites[ (int)ScreenType.GameOver ].enabled = false;
+
+		for( int i = 0; i < screenSprites.Length; i++ )
+			screenSprites[i].enabled = spriteEnabled[i];
 	}
 }
